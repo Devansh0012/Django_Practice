c68d1569ea6358a768e97966dceb8be8cd06cdc1
Devansh0012 <devanshdubey0012@gmail.com>
2024-06-20 04:04:22 +0530

    added Django Forms

--- DjangoProj/chai/admin.py
+++ DjangoProj/chai/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
+from .models import chaiVariety, ChaiReview, Store, chaiCertificate
 
 # Register your models here.
+class ChaiReviewInline(admin.TabularInline):
+    model = ChaiReview
+    extra = 1
+
+class chaiCertificateInline(admin.StackedInline):
+    model = chaiCertificate
+    extra = 1
+    
+class chaiVarietyAdmin(admin.ModelAdmin):
+    inlines = [ChaiReviewInline, chaiCertificateInline]
+    list_display = ('name', 'type', 'price')
+    search_fields = ['name', 'type']
+    list_filter = ['type']
+
+class StoreAdmin(admin.ModelAdmin):
+    list_display = ('name', 'location')
+    filter_horizontal = ('chai_varities',)
+
+admin.site.register(chaiVariety, chaiVarietyAdmin)
+admin.site.register(Store, StoreAdmin)  
--- DjangoProj/chai/models.py
+++ DjangoProj/chai/models.py
@@ -1,3 +1,54 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
 
 # Create your models here.
+class chaiVariety(models.Model):
+    CHAI_TYPE_CHOICE = [
+        ('ML', 'Masala'),
+        ('GC', 'Ginger'),
+        ('CC', 'Cardamom'),
+        ('KC', 'Kashmiri'),
+    ]
+    name = models.CharField(max_length=100)
+    image = models.ImageField(upload_to='images/')
+    date_added = models.DateTimeField(default=timezone.now)
+    type = models.CharField(max_length=2, choices=CHAI_TYPE_CHOICE, default='ML')
+    description = models.TextField(default='')
+    price = models.IntegerField(default=0)
+    
+    def __str__(self):
+        return self.name
+    
+# One to Many relationship
+
+class ChaiReview(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    chai = models.ForeignKey(chaiVariety, on_delete=models.CASCADE, related_name='reviews')
+    review = models.TextField()
+    rating = models.IntegerField()
+    date_added = models.DateTimeField(default=timezone.now)
+    
+    def __str__(self):
+        return f'{self.user.username} - {self.chai.name} - {self.rating}'
+    
+# Many to Many relationship
+
+class Store(models.Model):
+    name = models.CharField(max_length=100)
+    location = models.CharField(max_length=100)
+    chai_varities = models.ManyToManyField(chaiVariety, related_name='stores')
+    
+    def __str__(self):
+        return self.name
+    
+# One to One relationship
+
+class chaiCertificate(models.Model):
+    chai = models.OneToOneField(chaiVariety, on_delete=models.CASCADE, related_name='certificate')
+    certificate_number = models.CharField(max_length=100)
+    issue_date = models.DateTimeField(default=timezone.now)
+    valid_till = models.DateTimeField()
+    
+    def __str__(self):
+        return f'Certificate for {self.chai.name} - {self.certificate_number}'
